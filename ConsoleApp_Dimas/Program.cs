5f429d050db151045509ef3af300e8d03eb232c7
User <User@DESKTOP-RBMPGAB>
2023-09-11 11:30:13 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp_Dimas/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class UpdateChecker
+{
+    public static void Main()
+    {
+        // Проверка наличия и установка обновлений операционной системы
+        CheckAndInstallOSUpdates();
+
+        // Проверка наличия и установка обновлений приложений
+        CheckAndInstallAppUpdates();
+    }
+
+    private static void CheckAndInstallOSUpdates()
+    {
+        Console.WriteLine("Проверка наличия и установка обновлений операционной системы...");
+
+        // Код для проверки наличия и установки обновлений операционной системы
+        // В зависимости от платформы, этот код может отличаться
+
+        // Пример для Windows
+        var process = System.Diagnostics.Process.Start("cmd", "/C wuauclt /detectnow /updatenow");
+
+        // Пример для macOS
+        // var process = System.Diagnostics.Process.Start("/usr/sbin/softwareupdate", "-l -a");
+
+        // Ожидание завершения операции
+        process.WaitForExit();
+
+        // Получение кода возврата
+        int exitCode = process.ExitCode;
+
+        // Обработка результата и вывод информации
+        if (exitCode == 0)
+        {
+            Console.WriteLine("Обновления операционной системы установлены.");
+        }
+        else
+        {
+            Console.WriteLine("Установка обновлений операционной системы не удалась.");
+        }
+
+        Console.WriteLine("Проверка и установка обновлений операционной системы завершена.");
+    }
+
+    private static void CheckAndInstallAppUpdates()
+    {
+        Console.WriteLine("Проверка наличия и установка обновлений приложений...");
+
+        // Код для проверки наличия и установки обновлений приложений
+        // В зависимости от приложений, этот код может отличаться
+
+        // Пример для .NET приложений, использующих NuGet
+        var process = System.Diagnostics.Process.Start("dotnet", "restore");
+
+        // Ожидание завершения операции
+        process.WaitForExit();
+
+        // Получение кода возврата
+        int exitCode = process.ExitCode;
+
+        // Обработка результата и вывод информации
+        if (exitCode == 0)
+        {
+            Console.WriteLine("Обновления приложений установлены.");
+        }
+        else
+        {
+            Console.WriteLine("Установка обновлений приложений не удалась.");
+        }
+
+        Console.WriteLine("Проверка и установка обновлений приложений завершена.");
+    }
+}
